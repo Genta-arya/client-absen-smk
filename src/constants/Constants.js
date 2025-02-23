8e315cce05c894743e960e9925e2e00e72434590
Genta-arya <mgentaarya@gmail.com>
2025-02-23 14:47:32 +0700

    add foto laporan

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -37,7 +37,7 @@ export const formatDate = (dateString) => {
 };
 
 export let API_URL;
-const type = "productions";
+const type = "production";
 
 if (type === "production") API_URL = "https://pkl.apiservices.my.id/api";
 else API_URL = "http://localhost:8081/api";
